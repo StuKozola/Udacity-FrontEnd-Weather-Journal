07728fc916672155579911bbe34a24e417c72e5c
StuKozola <stuart.kozola@gmail.com>
2022-08-19 12:41:00 +0000

    fixed month calc issue

--- website/app.js
+++ website/app.js
@@ -15,7 +15,7 @@ const content = document.getElementById('content');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = 1+d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 /* Functions */
 // Function to validate the zip code
